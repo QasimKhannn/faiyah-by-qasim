2c73cbf2604fc7cb75d6960062b7670a99b0239c
QasimKhannn <iamqasimk@gmail.com>
2024-09-30 00:49:33 +0500

    Image Otp

--- components/home-comps/image-box.tsx
+++ components/home-comps/image-box.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import myImage from "../../public/my-image.jpg";
+import myImage from "../../public/my-image.webp";
 import AnimatedBorder from "../anim-border";
 
 interface props {
@@ -10,13 +10,14 @@ interface props {
 
 const ImageBox: React.FC<props> = ({ className }: props) => {
     return (
-        <div className={`${className} relative w-full h-80 md:h-full`}>
+        <div className={`${className} relative w-full min-h-[230px] md:h-full`}>
             <AnimatedBorder>
                 <Image
                     src={myImage}
                     alt="A description of the image for accessibility"
-                    fill
-                    className="object-cover rounded-2xl z-20 p-[0.14rem]"
+                    height={100}
+                    width={1000}
+                    className="object-cover h-full rounded-2xl z-20 p-[0.14rem]"
                     loading="lazy"
                     sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
                 />
