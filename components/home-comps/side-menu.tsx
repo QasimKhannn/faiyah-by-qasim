0429e12f87d0c54c3a7aa75cd5fd15795cf828d5
QasimKhannn <iamqasimk@gmail.com>
2024-09-30 18:49:22 +0500

    pushed force

--- components/home-comps/side-menu.tsx
+++ components/home-comps/side-menu.tsx
@@ -36,13 +36,9 @@ const SideMenu: React.FC<props> = ({ className }: props) => {
                     }}
                     className="z-50 flex flex-col justify-center items-center"
                 >
-                    <motion.p className="font-bold text-xl md:text-1xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
-                        The coder that nobody <br /> nobody asked for <br /> but everyone deserves
+                    <motion.p className="font-bold text-xl md:text-2xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+                        May the code <br /> be with you
                     </motion.p>
-                    {/* <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-                        <span>Join now →</span>
-                        <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-                    </button> */}
                 </motion.div>
             </ImagesSlider>
         </div >
